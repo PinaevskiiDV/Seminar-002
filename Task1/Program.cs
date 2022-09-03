24ac980a16979171759a18d681cdeabaecad84a0
Dmitrii <pinochet37@mail.ru>
2022-09-03 13:12:57 +0300

    created project Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите трёхзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+int SecondPosition(int num)
+{
+    int result = num / 10 - num / 100 * 10;
+    return(result);
+}
+Console.Write("Второе число ");
+Console.WriteLine(SecondPosition(number));
