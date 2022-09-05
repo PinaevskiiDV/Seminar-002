9a05540b6023e3e2291573d2b336d055186150dd
Dmitrii <pinochet37@mail.ru>
2022-09-05 14:26:08 +0300

    created project Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите цифру дня недели от 1 до 7");
+int numberA = Convert.ToInt32(Console.ReadLine());
+
+bool Multiple(int num)
+{
+    if(num == 6 || num == 7)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+if(Multiple(numberA)==true)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+}
+
